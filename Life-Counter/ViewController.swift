15a1cb7354a693701a8b051716dbda7759e41fdb
jcamato <amatojonathan@gmail.com>
2015-04-20 21:47:57 -0700

    simple life counter and game tracker

--- Life-Counter/ViewController.swift
+++ Life-Counter/ViewController.swift
@@ -10,16 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var player1Life: Int = 20
+    var player2Life: Int = 20
+    var player1Games: Int = 0
+    var player2Games: Int = 0
+
+    @IBOutlet weak var player1LifeLabel: UILabel!
+    
+    @IBOutlet weak var player2LifeLabel: UILabel!
+    
+    @IBOutlet weak var player1GamesLabel: UILabel!
+    
+    @IBOutlet weak var player2GamesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func resetGames(sender: AnyObject) {
+        player1Games = 0
+        player1GamesLabel.text = "\(player1Games)"
+        player2Games = 0
+        player2GamesLabel.text = "\(player2Games)"
     }
-
+    
+    @IBAction func resetLife(sender: AnyObject) {
+        player1Life = 20
+        updatePlayer1Life()
+        player2Life = 20
+        updatePlayer2Life()
+    }
+    
+    func updatePlayer1Life() {
+        player1LifeLabel.text = "\(player1Life)"
+        if (player1Life == 0) {
+            player1Games += 1
+            player1GamesLabel.text = "\(player1Games)"
+        }
+    }
+    
+    func updatePlayer2Life() {
+        player2LifeLabel.text = "\(player2Life)"
+        if (player2Life == 0) {
+            player2Games += 1
+            player2GamesLabel.text = "\(player2Games)"
+        }
+    }
+    
+    @IBAction func p1Plus(sender: AnyObject) {
+        if (player1Life != 0) {
+            player1Life += 1
+            updatePlayer1Life()
+        }
+    }
+    
+    @IBAction func p1Minus(sender: AnyObject) {
+        if (player1Life != 0) {
+            player1Life -= 1
+            updatePlayer1Life()
+        }
+    }
+    
+    @IBAction func p2Plus(sender: AnyObject) {
+        if (player2Life != 0) {
+            player2Life += 1
+            updatePlayer2Life()
+        }
+    }
+    
+    @IBAction func p2Minus(sender: AnyObject) {
+        if (player2Life != 0) {
+            player2Life -= 1
+            updatePlayer2Life()
+        }
+    }
+    
+    
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return false
+    }
 
 }
 
