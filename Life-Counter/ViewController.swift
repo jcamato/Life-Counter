568994062c246ed9e94011165cacdc15f94e995b
jcamato <amatojonathan@gmail.com>
2015-04-20 20:12:40 -0700

    initial project

---
+++ Life-Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Life-Counter
+//
+//  Created by Jonathan Amato on 4/20/15.
+//  Copyright (c) 2015 Jonathan Amato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
